15a918ebbd0507ade9f17966b29db9d49de66019
Sergio Miranda <152328184+TitanDevelopMan@users.noreply.github.com>
2024-11-01 18:12:00 -0300

    eslint prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-   <h1>Welcome a next js</h1>
+    <h1 className=" text-3xl font-black text-white">Welcome a next js</h1>
   );
 }
